93d9703a7a3197f3036794f766d1fbaeeb1ed080
larsericsson <larserics@gmail.com>
2014-03-18 22:47:37 -0400

    make it happen

--- whereami.js
+++ whereami.js
@@ -1,4 +1,12 @@
 (function(window) {
-  $waitlistEl = $('#rsvp-list-waitlist');
-  console.log("loaded");
+  var $waitlistEl = $('#rsvp-list-waitlist');
+  var myId = Meetup.Data.meetstacheRsvpEvent.Member.memberId;
+  var $myRsvpEl = $('[data-memberid="' + myId + '"');
+  var myPos = $waitlistEl.children().index($myRsvpEl) + 1;
+
+  if (myPos >= 0) {
+    alert("You're number " + myPos + " on the waitlist");
+  } else {
+    alert("Couldn't find you on the waitlist");
+  }
 })(window);
